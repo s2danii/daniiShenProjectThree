6916684cf450d8ebddd9bbf4b391aed6eed0bc86
Danii Shen <s2d.shen@gmail.com>
2019-08-21 00:38:40 -0400

    first version of javascript for number section

--- scripts/script.js
+++ scripts/script.js
@@ -14,18 +14,58 @@ cootieCatcher.finalAnswers = {
 
 // console.log(numberChoices.numberChoiceTwo);
 
-
-// cootieCatcher.smoothScroll = function () {
-    $(`.catcherSquare`).on(`click`, function (event) {
+// SMOOTH SCROLL FUNCTION DEFINED
+cootieCatcher.smoothScroll = function (clickedElement) {
+    $(clickedElement).on(`click`, function (event) {
         if(this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
             $(`html, body`).animate({
                 scrollTop: $(hash).offset().top
-            }, 900, function() {
+            }, 800, function() {
                 window.location.hash = hash;
             });
         }
     });
-// }
+};
+
+cootieCatcher.forLoop = function (arraySelected) {
+    for(let n = 0; n < 4; n++) {
+        $(`li:nth-of-type(${n+1}) .numberSquare`).text(`${arraySelected[n]}`);
+    };
+};
+
+cootieCatcher.nextToShow = function (section) {
+    $(section).on(`click`, function() {
+        let selectedSquare = $(this).children(`div`).attr(`class`);
+        if (selectedSquare === `odd`) {
+            let numbersDisplayed = cootieCatcher.numberChoices.numberChoiceOne;
+            cootieCatcher.forLoop(numbersDisplayed);
+        } else {
+            let numbersDisplayed = cootieCatcher.numberChoices.numberChoiceTwo;
+            cootieCatcher.forLoop(numbersDisplayed);
+        };
+        
+    })
+};
+
+cootieCatcher.numberPick = function () {
+    
+}
+
+
+
+// INIT FUNCTION DEFINED
+cootieCatcher.init = function () {
+    cootieCatcher.smoothScroll(`.catcherSquare`);
+    cootieCatcher.smoothScroll(`.startButton`);
+    cootieCatcher.nextToShow(`.colorSquare`);
+}
+
+$(document).ready(function() {
+    cootieCatcher.init();
+});
+
+
+
 
