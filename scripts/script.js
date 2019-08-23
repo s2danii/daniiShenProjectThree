205555cf5d02e167f3db880ab3b2136b90fc0466
Danii Shen <s2d.shen@gmail.com>
2019-08-23 15:39:46 -0400

    got card flip to work!!!!

--- scripts/script.js
+++ scripts/script.js
@@ -22,22 +22,25 @@ cootieCatcher.smoothScroll = function (clickedElement) {
     $(clickedElement).on(`click`, function (event) {
         if(this.hash !== "") {
             event.preventDefault();
-            const hash = this.hash;
-            $(`html, body`).animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-                window.location.hash = hash;
+            const locationHash = this.hash;
+            $(`html`).animate({
+                scrollTop: $(locationHash).offset().top
+            }, 800, `swing`, function() {
+                window.location.hash = locationHash;
+                cootieCatcher.flipSquares(locationHash);
             });
         }
     });
+    
 };
 
 // FUNCTION TO PRINT ARRAY ONTO SQUARES IN HTML DEFINED HERE
-cootieCatcher.forLoop = function (sectionToAppear, arraySelected) {
+cootieCatcher.printArray = function (sectionToAppear, arraySelected) {
     arraySelected.forEach(function (arrayItem, arrayIndex) {
         $(`li:nth-of-type(${arrayIndex + 1}) ${sectionToAppear}`).text(`${arrayItem}`);
     });
 };
+
 
 
 // FUNCTION TO DECIDE THE ARRAY TO POPULATE THE NEXT SECTION DEFINED HERE
@@ -47,6 +50,7 @@ cootieCatcher.nextToShow = function (sectionSquare) {
         let questionSection = ``;
         let nextSection = ``;
         
+        // To determine the array to display in the next set of questions for colour choice and number choice
         if (sectionSquare === `.colorSquare`) {
             selectedSquare = $(this).children(`div`).attr(`class`);
             questionSection = cootieCatcher.numberChoices;
@@ -62,12 +66,13 @@ cootieCatcher.nextToShow = function (sectionSquare) {
             }
         };
 
+
         if (selectedSquare === `odd`) {
             let setDisplayed = questionSection[0];
-            cootieCatcher.forLoop(nextSection, setDisplayed);
+            cootieCatcher.printArray(nextSection, setDisplayed);
         } else {
             let setDisplayed = questionSection[1];
-            cootieCatcher.forLoop(nextSection, setDisplayed);
+            cootieCatcher.printArray(nextSection, setDisplayed);
         };
         
     })
@@ -95,6 +100,13 @@ cootieCatcher.restart = function () {
     }
 )};
 
+// CARD FLIP MAGIC DEFINED HERE
+cootieCatcher.flipSquares = function(sectiontoFlip) {
+        
+        $(`${sectiontoFlip} .card`).toggleClass(`isFlipped`);
+
+}
+
 
 
 
@@ -103,10 +115,11 @@ cootieCatcher.restart = function () {
 cootieCatcher.init = function () {
     cootieCatcher.smoothScroll(`.catcherSquare`);
     cootieCatcher.smoothScroll(`.startButton`);
+    
     cootieCatcher.nextToShow(`.colorSquare`);
     cootieCatcher.nextToShow(`.numberSquare`);
     cootieCatcher.bigReveal();
-    // cootieCatcher.smoothScroll(`.restartButton`);
+    cootieCatcher.smoothScroll(`.restartButton`);
     cootieCatcher.restart();
     
 }
