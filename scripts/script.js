112d7f54a158c7850171d91309f4878dff8943b7
Danii Shen <s2d.shen@gmail.com>
2019-08-24 22:31:37 -0400

    added form to front page

--- scripts/script.js
+++ scripts/script.js
@@ -25,10 +25,12 @@ cootieCatcher.printArray = function (sectionToAppear, arraySelected) {
 // FUNCTION TO DECIDE THE ARRAY TO POPULATE THE NEXT SECTION DEFINED HERE
 cootieCatcher.nextToShow = function (sectionSquare) {
     $(sectionSquare).on(`click`, function() {
+        // square that is clicked by user
         let selectedSquare = ``;
+        // the array that will populate the next question section
         let questionSection = ``;
+        // the upcoming section that needs to be populated
         let nextSection = ``;
-        
         // To determine the array to display in the next set of questions for colour choice and number choice
         if (sectionSquare === `.colorSquare`) {
             selectedSquare = $(this).children(`div`).attr(`class`);
@@ -44,7 +46,7 @@ cootieCatcher.nextToShow = function (sectionSquare) {
                 selectedSquare = `odd`;
             }
         };
-
+        // user chooses either an odd or even number/colour (colour has odd or even # of letters). Based on odd or even, this determines the item to print from the array
         if (selectedSquare === `odd`) {
             let setDisplayed = questionSection[0];
             cootieCatcher.printArray(nextSection, setDisplayed);
@@ -60,7 +62,6 @@ cootieCatcher.nextToShow = function (sectionSquare) {
 cootieCatcher.bigReveal = function () {
     $(`.tabSquare`).on(`click`, function() {
         let cardIndex = Math.floor(Math.random() * 10);
-        console.log(cardIndex);
         $(`.resultsAnswer`).html(`"${cootieCatcher.finalAnswers[cardIndex]}"`);
     });
 };
@@ -74,6 +75,7 @@ cootieCatcher.restart = function () {
         }, 1000, function () {
                 location.hash = ``;
                 location.reload();
+                $(`:text`).val(``);
         });
     }
 )};
@@ -100,6 +102,29 @@ cootieCatcher.smoothScroll = function (clickedElement) {
     });
 };
 
+// FUNCTION TO STORE USER INPUT AND PRINT AT THE END
+cootieCatcher.startGame = function () {
+    $(`form`).on(`submit`, function (event) {
+        event.preventDefault();
+        const locationHash = $(`:submit`).data(`hash`);
+        let userQuestion = $(`input`).val();
+        // console.log(userQuestion);
+        // console.log(locationHash);
+        $(`html`).animate({
+            scrollTop: $(locationHash).offset().top
+        }, 800, `swing`, function () {
+            window.location.hash = locationHash;
+            cootieCatcher.printQuestion(userQuestion);
+        });
+    }
+)};
+
+// FUNCTION TO PRINT USER QUESTION
+cootieCatcher.printQuestion = function (userQuestion) {
+    $(`.userQuestion`).html(`"${userQuestion}"`);
+};
+
+
 // REFRESH FUNCTION TO CLEAR OUT HASH DEFINED HERE
 
 cootieCatcher.clearHash = function () {
@@ -113,7 +138,8 @@ cootieCatcher.clearHash = function () {
 // INIT FUNCTION DEFINED HERE
 cootieCatcher.init = function () {
     cootieCatcher.smoothScroll(`.catcherSquare`);
-    cootieCatcher.smoothScroll(`.startButton`);
+    // cootieCatcher.smoothScroll(`.startButton`);
+    cootieCatcher.startGame();
     cootieCatcher.nextToShow(`.colorSquare`);
     cootieCatcher.nextToShow(`.numberSquare`);
     cootieCatcher.bigReveal();
@@ -126,4 +152,5 @@ cootieCatcher.init = function () {
 
 $(document).ready(function() {
     cootieCatcher.init();
+    
 });
